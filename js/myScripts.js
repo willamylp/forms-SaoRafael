9991048920adb9878cde8785f3e74ebdd0994806
willamylp <willamy.wlp@gmail.com>
2020-12-16 23:16:16 -0300

    Criando Form termoIsolamento

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,39 +1,39 @@
 
 
-$(document).ready(function () {
-    $("#id_CEP").mask("00000-000");
-});
-$(function () {
-    var input = $('#id_telefone');
-    input.on('focusin', function () {
-        input.maxLength = "11";
-        input.mask('00000000000')
-    });
-    input.on('focusout', function () {
-        input.maxLength = "15";
-        input.mask('(00)0 0000.0000')
-    });
-})
-$(function () {
-    var input = $('#id_cns');
-    input.on('focusin', function () {
-        input.maxLength = "15";
-        input.mask('000000000000000', { reverse: true });
-    });
-    input.on('focusout', function () {
-        input.maxLength = "18";
-        input.mask('000 0000 0000 0000', { reverse: true });
-    });
-});
+// $(document).ready(function () {
+//     $("#id_CEP").mask("00000-000");
+// });
+// $(function () {
+//     var input = $('#id_telefone');
+//     input.on('focusin', function () {
+//         input.maxLength = "11";
+//         input.mask('00000000000')
+//     });
+//     input.on('focusout', function () {
+//         input.maxLength = "15";
+//         input.mask('(00)0 0000.0000')
+//     });
+// })
+// $(function () {
+//     var input = $('#id_cns');
+//     input.on('focusin', function () {
+//         input.maxLength = "15";
+//         input.mask('000000000000000', { reverse: true });
+//     });
+//     input.on('focusout', function () {
+//         input.maxLength = "18";
+//         input.mask('000 0000 0000 0000', { reverse: true });
+//     });
+// });
 
-$(function () {
-    var input = $('#id_cartaoSUS');
-    input.on('focusin', function () {
-        input.maxLength = "15";
-        input.mask('000000000000000', { reverse: true });
-    });
-    input.on('focusout', function () {
-        input.maxLength = "18";
-        input.mask('000 0000 0000 0000', { reverse: true });
-    });
-});
+// $(function () {
+//     var input = $('#id_cartaoSUS');
+//     input.on('focusin', function () {
+//         input.maxLength = "15";
+//         input.mask('000000000000000', { reverse: true });
+//     });
+//     input.on('focusout', function () {
+//         input.maxLength = "18";
+//         input.mask('000 0000 0000 0000', { reverse: true });
+//     });
+// });
