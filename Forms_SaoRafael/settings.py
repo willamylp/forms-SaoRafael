b19cfff42be849555230e4c6e7f358792f0d87e0
willamylp <willamy.wlp@gmail.com>
2021-04-03 20:36:22 -0300

    Bug Resolving Settings.py

--- Forms_SaoRafael/settings.py
+++ Forms_SaoRafael/settings.py
@@ -148,10 +148,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    'estaticos',
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'estaticos')
+
+if DEBUG: 
+    STATICFILES_DIRS = [
+        'estaticos',
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = [
+#     'estaticos',
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # LOGIN
